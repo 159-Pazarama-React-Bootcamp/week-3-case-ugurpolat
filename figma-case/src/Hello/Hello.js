6df540f0b495b414067b6f1daaf12319bd0b3b68
Ugur Polat <ugurpolat113@gmail.com>
2022-01-07 15:02:50 +0300

    Add Hello page.

---
+++ figma-case/src/Hello/Hello.js
@@ -0,0 +1,50 @@
+import logo from "../Frame.svg";
+import React, { Component } from "react";
+import "./Hello.css";
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container-hello">
+          <div className="text-area">
+            <h1>Hello 👋</h1>
+            <p>
+              I hope everyone is safe and sound. I designed different type of
+              lending pages,application. it can help others to develop more
+              ideas from this. I keep it simple and minimal. It can also help
+              you find different options in exploring and improving your skills.
+            </p>
+            <p>
+              I am available for new projects. I hope you show me your support
+              😄
+            </p>
+            <p>
+              I wish you luck, <br></br>
+              Drax ❤️
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="arrow-down"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M16 17l-4 4m0 0l-4-4m4 4V3"
+              />
+            </svg>
+            <button className="button-coffee">
+              <a className="underline">Buy me a Coffee</a>
+            </button>
+          </div>
+          <div className="container-logo">
+            <img className="logo-img" src={logo} alt="logo"></img>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
